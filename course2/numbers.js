6f5a46f3e8d253a073f921e43710d178d714aead
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2022-07-03 15:06:42 -0300

    -

--- course2/numbers.js
+++ course2/numbers.js
@@ -6,5 +6,5 @@ let num2 = 2.5;
 // console.log(num1.toString(2))
 // console.log(num1.toFixed(5))
 // console.log(Number.isInterger(num1))
-let temp = num1 * 'Ola'
+let temp = num1 * '5'
 console.log(Number,isNaN(temp))
