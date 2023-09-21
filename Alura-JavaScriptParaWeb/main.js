281449c06edfecb4d6700f834fe933caa0e6acb7
Ariel Pellegrino <ariel.pell@gmail.com>
2023-09-20 21:44:56 -0300

    upload - projeto full

---
+++ Alura-JavaScriptParaWeb/main.js
@@ -0,0 +1,27 @@
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
