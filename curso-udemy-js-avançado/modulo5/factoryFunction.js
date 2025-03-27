8f217dd544872c72d941dd687ede8e585e02d9af
Ariel Pellegrino <ariel.pell@gmail.com>
2025-03-27 20:28:36 -0300

    update: mod5

--- curso-udemy-js-avançado/modulo5/factoryFunction.js
+++ curso-udemy-js-avançado/modulo5/factoryFunction.js
@@ -1,20 +1,24 @@
-function criaPessoa(nome, sobrenome,a,p) {
+function criaPessoa(nome, sobrenome, a, p) {
     return {
         nome,
         sobrenome,
+        nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        },
         fala: function (assunto) {
             return `${this.nome} está ${this.assunto}.`;
         },
         altura: a,
         peso: p,
-        imc(){
-            const
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
         }
     };
 }
 
 const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
-const p1 = criaPessoa('Maria', 'Joaquina', 1.6, 42);
-/*console.log(p1)
-console.log('</br>')*/
-console.log(p1.pergunta('JavaScript'))
+console.log(p1.imc)
+
+const p2 = criaPessoa('Maria', 'Joaquina', 1.6, 42);
+console.log(p2.imc)
